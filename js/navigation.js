6f5e5b4444d89bfdf7b837ee3be4d3051719ef95
SaidElHachmy <medsaidmdelhachmy@gmail.com>
2026-04-28 21:47:35 +0000

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -9,22 +9,23 @@ function showSection(sectionId) {
   if (target) {
     target.classList.add("active");
   }
+  
+  
+  
+    // 🔥 RESET when leaving years
+  if (sectionId !== "years" && typeof resetSchoolUI === "function") {
+    resetSchoolUI();
+  }
 
   // 📚 LOAD LIBRARY WHEN OPENED
   if (sectionId === "library") {
     loadBooks();
   }
+
+  if (sectionId === "staff" && window.loadStaff) {
+  loadStaff();
 }
-function showSection(sectionId) {
-  const sections = document.querySelectorAll(".section");
-
-  sections.forEach(sec => sec.classList.remove("active"));
-
-  const target = document.getElementById(sectionId);
-  if (target) target.classList.add("active");
-
-  // 🔥 RESET when leaving years
-  if (sectionId !== "years" && typeof resetSchoolUI === "function") {
-    resetSchoolUI();
-  }
+  
+  
+  
 }
