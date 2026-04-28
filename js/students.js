65552ec8c8508fe6127e62f8ca9d2557a6644208
SaidElHachmy <medsaidmdelhachmy@gmail.com>
2026-04-28 08:11:39 +0000

    Update students.js

--- js/students.js
+++ js/students.js
@@ -110,33 +110,32 @@ function loadStudentDetails(id) {
   container.style.display = "block";
 
   container.innerHTML = `
-    <div class="student-profile-card">
+  <div class="student-profile-card" dir="${lang === "ar" ? "rtl" : "ltr"}">
 
-      <img src="${img}" onerror="this.src='assets/images/error.png'">
+    <img src="${img}" onerror="this.src='assets/images/error.png'">
 
-      <h2>${get(student.name)}</h2>
+    <h2>${get(student.name)}</h2>
 
-      <div class="student-info">
+    <div class="student-info">
 
+      <p> <span style="font-weight: bold;">🆔 ${lang === "ar" ? "الرقم المعرف" : lang === "fr" ? "ID" : "ID"}:</span> ${get(student.nationalId)}</p>
 
-        <p><strong>${lang === "ar" ? "الرقم المعرف": "ID"}:</strong> ${get(student.nationalId)}</p>
+      <p> <span style="font-weight: bold;">🎂 ${lang === "ar" ? "تاريخ الميلاد" : lang === "fr" ? "Date de naissance" : "Birth Date"}:</span> ${get(student.birthDate)}</p>
 
-        <p><strong>${lang === "ar" ? "تاريخ الولادة" : "Birth Date"}:</strong> ${get(student.birthDate)}</p>
+      <p> <span style="font-weight: bold;">🏫 ${lang === "ar" ? "رقم المدرسة" : lang === "fr" ? "Numéro scolaire" : "School Number"}:</span> ${get(student.schoolNumber)}</p>
 
-        <p><strong>${lang === "ar" ? "الرقم المدرسي" : "School Number"}:</strong> ${get(student.schoolNumber)}</p>
+      <p> <span style="font-weight: bold;">📞 ${lang === "ar" ? "الهاتف" : lang === "fr" ? "Téléphone" : "Phone"}:</span> ${get(student.phone)}</p>
 
-        <p><strong>${lang === "ar" ? "الهاتف" : "Phone"}:</strong> ${get(student.phone)}</p>
+      <p> <span style="font-weight: bold;">📍 ${lang === "ar" ? "العنوان" : lang === "fr" ? "Adresse" : "Address"}:</span> ${get(student.address)}</p>
 
-        <p><strong>${lang === "ar" ? "العنوان" : "Address"}:</strong> ${get(student.address)}</p>
+      <p> <span style="font-weight: bold;">🎓 ${lang === "ar" ? "المستوى" : lang === "fr" ? "Niveau" : "Level"}:</span> ${get(student.level)}</p>
 
-        <p><strong>${lang === "ar" ? "المستوى" : "Level"}:</strong> ${get(student.level)}</p>
+      <p> <span style="font-weight: bold;">📚 ${lang === "ar" ? "الدورة" : lang === "fr" ? "Cycle" : "Cycle"}:</span> ${get(student.cycle)}</p>
 
-        <p><strong>${lang === "ar" ? "الدورة" : "Cycle"}:</strong> ${get(student.cycle)}</p>
-
-
-
-      </div>
 
     </div>
-  `;
+
+  </div>
+`;
+  
 }
