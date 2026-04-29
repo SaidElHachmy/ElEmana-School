875a4d5873985f527f7d71d7c23f515a63cb61f7
SaidElHachmy <medsaidmdelhachmy@gmail.com>
2026-04-29 21:05:48 +0000

    Update yearsData.js

--- data/yearsData.js
+++ data/yearsData.js
@@ -1,18 +1,28 @@
 window.yearsData = [
+  
   {
     id: 1,
-    name: {
-      en: "School year : 2024 - 2025",
-      ar: "السنة الدراسية : 2024 - 2025",
-      fr: "Année scolaire : 2024 - 2025"
-    }
-  },
-  {
-    id: 2,
     name: {
       en: "School year : 2025 - 2026",
       ar: "السنة الدراسية : 2025 - 2026",
       fr: "Année scolaire : 2025 - 2026"
     }
+  },
+  
+  
+  /*
+    {
+    id: 2,
+    name: {
+      en: "School year : 2026 - 2027",
+      ar: "السنة الدراسية : 2026 - 2027",
+      fr: "Année scolaire : 2026 - 2027"
+    }
   }
+  
+  */
+  
+  
+  
+  
 ];
